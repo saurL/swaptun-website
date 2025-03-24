e6d5bfbf0dbb4aede4ac5ef2438caac2916c3b2e
Lucas Sauron <sauronlucas@gmail.com>
2025-03-24 13:07:48 +0100

    added change to header and footer

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -11,34 +11,8 @@
 </script>
 
 <style scoped>
-.footer {
-  background-color: #f4f4f4;
-  padding: 20px;
-  text-align: center;
-  display: flex;
-  justify-content: space-between;
-}
-
-.footer-content {
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-
-.footer-menu {
-  width: fit-content;
-  display: inline-flex;
-  margin: 0;
-  padding: 0;
-}
-
-.el-menu-item {
-  color: #333;
-  font-size: 14px;
-}
-
-.el-menu-item:hover {
-  color: #409eff;
+@import "~/assets/css/main.css";
+footer {
+  @apply bg-blue-500 text-white p-4;
 }
 </style>
--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -4,42 +4,11 @@
   </header>
 </template>
 
-<script setup>
-import { ref } from "vue";
-
-// Utilisation de ref pour définir l'élément du menu actif
-const activeMenu = ref("1");
-</script>
+<script setup></script>
 
 <style scoped>
-/* Style personnalisé pour le header */
-.el-header {
-  background-color: #409eff;
-  padding: 10px;
-  display: flex;
-
-  justify-content: space-between;
-  height: auto;
-}
-
-.logo {
-  font-size: 24px;
-  color: white;
-  font-weight: bold;
-}
-
-.el-menu-demo {
-  display: flex;
-  flex-wrap: nowrap;
-  background-color: transparent;
-}
-
-.el-menu-item {
-  min-width: auto;
-  color: white;
-}
-
-.el-menu-item:hover {
-  background-color: #66b1ff;
+@import "~/assets/css/main.css";
+header {
+  @apply bg-blue-500 text-white p-4;
 }
 </style>
